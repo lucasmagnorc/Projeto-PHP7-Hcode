208a1abc1308d22ef3eefad5e41c4f902bed2f38
Lucas Magno <lucasmagnorc@gmail.com>
2019-01-15 11:00:50 -0200

    created admin page and tpl

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page{
+    public function __construct($opts = array(), $tpl_dir = '/Projeto-PHP7-Hcode/views/admin/'){
+        parent::__construct($opts, $tpl_dir);
+    }
+}
+
+?>
