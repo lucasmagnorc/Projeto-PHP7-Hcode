2399cef6695f016cc60f2c355a1ca5431b1e097b
Lucas Magno <lucasmagnorc@gmail.com>
2019-01-21 21:28:16 -0200

    Password recovery

--- index.php
+++ index.php
@@ -78,9 +78,9 @@ $app->get("/admin/users/:iduser", function($iduser){
 });
 
 $app->post('/admin/users/create', function(){
-	User::verifyLogin();
+	//User::verifyLogin();
 	$user = new User();
-	$_POST['inadmin'] = (isset($_POST['inadmin']))?1:0;
+	//$_POST['inadmin'] = (isset($_POST['inadmin']))?1:0;
 	$user->setData($_POST);
 	$user->save();
 	header('Location: /Projeto-PHP7-Hcode/index.php/admin/users');
@@ -97,6 +97,55 @@ $app->post('/admin/users/:iduser', function($iduser){
 	$user->update();
 	header('Location: /Projeto-PHP7-Hcode/index.php/admin/users');
 	exit;
+});
+
+$app->get('/admin/forgot', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl('forgot');
+});
+
+$app->post('/admin/forgot', function(){
+	$user = User::getForgot($_POST['email']);
+	header('Location: /Projeto-PHP7-Hcode/index.php/admin/forgot/sent');
+	exit;
+});
+
+$app->get('/admin/forgot/sent', function(){
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl('forgot-sent');
+});
+
+$app->get('/admin/forgot/reset', function(){
+	$user = User::validForgotDecrypt($_GET['code']);
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl('forgot-reset', array(
+		'name'=>$user['desperson'],
+		'code'=>$_GET['code']
+	));
+});
+
+$app->post('/admin/forgot/reset', function(){
+	$forgot = User::validForgotDecrypt($_POST['code']);
+	User::setForgotUsed($forgot['idrecovery']);
+	$user = new User();
+	$user->get((int)$forgot['iduser']);
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT, ["cost"=>12]);
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl('forgot-reset-success');
 });
 
 $app->run();
