9f3949a06cadc642bdf3efed9cb8ed6110de7c8f
Lucas Magno <lucasmagnorc@gmail.com>
2019-02-12 21:41:37 -0200

    Made web service for address

--- site.php
+++ site.php
@@ -94,13 +94,82 @@ $app->post('/cart/freight', function(){
 
 $app->get("/checkout", function(){
 	User::verifyLogin(false);
-	$cart = Cart::getFromSession();
 	$address = new Address();
+	$cart = Cart::getFromSession();
+	if(isset($_GET['zipcode'])){
+		$_GET['zipcode'] = $cart->getdeszipcode();
+	}
+
+	if(isset($_GET['zipcode'])){
+		$address->loadFromCEP($_GET['zipcode']);
+		$cart->setdeszipcode($_GET['zipcode']);
+		$cart->save();
+		$cart->getCalculateTotal();
+
+	}
+	if(!$address->getdesaddress()) $address->setdesaddress('');
+	if(!$address->getdescomplement()) $address->setdescomplement('');
+	if(!$address->getdesdistrict()) $address->setdesdistrict('');
+	if(!$address->getdescity()) $address->setdescity('');
+	if(!$address->getdesstate()) $address->setdesstate('');
+	if(!$address->getdescountry()) $address->setdescountry('');
+	if(!$address->getdeszipcode()) $address->setdeszipcode('');
+
 	$page = new Page();
 	$page->setTpl('checkout', [
 		'cart'=>$cart->getValues(),
-		'address'=>$address->getValues()
+		'address'=>$address->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Address::getMsgError()
 	]);
+});
+
+$app->post("/checkout", function(){
+	User::verifyLogin(false);
+	if(!isset($_POST['zipcode']) || $_POST['zipcode'] === ''){
+		Cart::setMsgError("Informe o CEP");
+		header("Location: /Projeto-PHP7-Hcode/index.php/checkout");
+		exit;
+	}
+
+	if(!isset($_POST['desaddress']) || $_POST['desaddress'] === ''){
+		Cart::setMsgError("Informe o endereço");
+		header("Location: /Projeto-PHP7-Hcode/index.php/checkout");
+		exit;
+	}
+	
+	if(!isset($_POST['desdistrict']) || $_POST['desdistrict'] === ''){
+		Cart::setMsgError("Informe o bairro");
+		header("Location: /Projeto-PHP7-Hcode/index.php/checkout");
+		exit;
+	}
+	
+	if(!isset($_POST['descity']) || $_POST['descity'] === ''){
+		Cart::setMsgError("Informe a cidade");
+		header("Location: /Projeto-PHP7-Hcode/index.php/checkout");
+		exit;
+	}
+	
+	if(!isset($_POST['desstate']) || $_POST['desstate'] === ''){
+		Cart::setMsgError("Informe o estado");
+		header("Location: /Projeto-PHP7-Hcode/index.php/checkout");
+		exit;
+	}
+	
+	if(!isset($_POST['descountry']) || $_POST['descountry'] === ''){
+		Cart::setMsgError("Informe o pais");
+		header("Location: /Projeto-PHP7-Hcode/index.php/checkout");
+		exit;
+	}
+
+	$user = User::getFromSession();
+	$address = new Address();
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+	$address->setData($_POST);
+	$address->save();
+	header("Location: /Projeto-PHP7-Hcode/index.php/order");
+	exit;
 });
 
 $app->get("/login", function(){
