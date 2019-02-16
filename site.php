8f1f7d02977b3a7a3e250298ca2e0a2468cc2a5e
Lucas Magno <lucasmagnorc@gmail.com>
2019-02-15 22:33:31 -0200

    Made alter password

--- site.php
+++ site.php
@@ -446,4 +446,47 @@ $app->get('/profile/orders/:idorder', function($idorder){
 	]);
 });
 
+$app->get('/profile/profile-change-password', function(){
+	User::verifyLogin(false);
+	$page = new Page();
+	$page->setTpl("profile-change-password", [
+		'changePassError'=>User::getError(),
+		'changePassSuccess'=>User::getSuccess()
+	]);
+});
+
+$app->post('/profile/change-password', function(){
+	User::verifyLogin(false);
+	if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+		User::setError("Digite a senha atual.");
+		Header("Location: /Projeto-PHP7-Hcode/index.php/profile/profile-change-password");
+		exit;
+	}
+
+	if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+		User::setError("Confirme a nova senha.");
+		Header("Location: /Projeto-PHP7-Hcode/index.php/profile/profile-change-password");
+		exit;
+	}
+
+	if ($_POST['current_pass'] === $_POST['new_pass_confirm']) {
+		User::setError("A sua nova senha deve ser diferente da atual.");
+		Header("Location: /Projeto-PHP7-Hcode/index.php/profile/profile-change-password");
+		exit;
+	}
+
+	$user = User::getFromSession();
+	if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+		User::setError("A senha está inválida.");
+		Header("Location: /Projeto-PHP7-Hcode/index.php/profile/profile-change-password");
+		exit;
+	}
+
+	$user->setdespassword($_POST['new_pass']);
+	$user->update();
+	$_SESSION[User::SESSION] = $user->getValues();
+	User::setSuccess("Senha alterada com sucesso.");
+	Header("Location: /Projeto-PHP7-Hcode/index.php/profile/profile-change-password");
+	exit;
+});
 ?>
